a812bae485f157bd02971f749391d74f7f7ae6dd
NQTrang1801 <trangnguyen180103@gmail.com>
2025-04-15 10:50:13 +0700

    TV000/feat: init docker and travelcore

---
+++ travelcore/src/main/java/com/bm/travelcore/TravelcoreApplication.java
@@ -0,0 +1,13 @@
+package com.bm.travelcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelcoreApplication.class, args);
+    }
+
+}
---
+++ travelcore/src/test/java/com/bm/travelcore/TravelcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm.travelcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
